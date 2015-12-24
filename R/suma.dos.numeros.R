b704048726dde10d625ea7084fede1a4c6dce803
ABFenix <abmorena@gmail.com>
2015-12-24 14:49:44 +0100

    libreriaR v-1.0.1
    Review help in suma.dos.numeros

--- R/suma.dos.numeros.R
+++ R/suma.dos.numeros.R
@@ -4,9 +4,9 @@
 #' @param y A number. Default value, 0
 #' @return The sum of {x} and {y}.
 #' @examples
-#' suma.dos.numeros(1, 1)    return 2
-#' suma.dos.numeros(10, 1)   return 11
-#' suma.dos.numeros(10)      return 10
-#' suma.dos.numeros(10.1)    return 10.1
+#' suma.dos.numeros(1, 1)
+#' suma.dos.numeros(10, 1)
+#' suma.dos.numeros(10)
+#' suma.dos.numeros(10.1)
 #' @export
 suma.dos.numeros <- function (x,y=0) return (x+y)
