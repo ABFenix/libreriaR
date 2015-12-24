8e13c9a563ca63a87c8af6fb443b8875db4220a6
ABFenix <abmorena@gmail.com>
2015-12-24 01:21:40 +0100

    libreriaR v-0.1.4
    Fixed problems with help function

--- R/matrix.zero.R
+++ R/matrix.zero.R
@@ -4,10 +4,10 @@
 #' @param m An integer. Default value, 1
 #' @return A zeros matrix. Dimensions: rows number = {n}; columns number = {m}.
 #' @examples
-#' matrix.zero(4, 5)    
-#' matrix.zero(4)   
-#' matrix.zero()      
-#' matrix.zero(5,4) 
+#' matrix.zero(4, 5)
+#' matrix.zero(4)
+#' matrix.zero()
+#' matrix.zero(5,4)
 #' @export   
 matrix.zero <- function (n=1,m=1) {
   c <- matrix(rep(0,n*m), nrow=n, ncol=m)
